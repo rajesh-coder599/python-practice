c46feb5a71a5a537aee12cceec209788808b25df
Rajesh Rayal <rayalrajesh6@gmail.com>
2026-02-20 11:13:18 +0530

    added basic python practice files: variables, operators, input, etc

---
+++ Data_types.py
@@ -0,0 +1,43 @@
+#integers
+a = 4
+b = -4
+c = 0
+#integers can be +ve 0 -ve
+print(a)
+print(b)
+print(c)
+
+
+#string
+d = "rajesh rayal"
+e = 'computer scince'
+f = '''python language'''
+#string can be stored in double  single or triple quataion
+print(d)
+print(e)
+print(f)
+
+
+#float
+g = 1.33
+h = 2.5
+i = 4.0
+#generaly decimal value called as float data type
+print(g)
+print(h)
+print(i)
+
+
+#boolean
+age = 24
+old = False
+young = True
+#boolean value stores only True & False statments (always T&F are cpital)
+print(type(old))
+print(type(young))
+
+
+#None
+j = None
+#if we dont want to store any data type in variable
+print(type(j)) 
